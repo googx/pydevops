b6547176ec042bed159f7ce99549187ccedd8dae
hx940929 <836845967@qq.com>
2019-02-28 13:06:04 +0800

    test尝试使用docker封装py. 使用shell来构建docker镜像.

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 #--------------------------------------------------
-# File Name: test.sh
+# File Name: build.sh
 # Author: hanxu
 # AuthorSite: http://www.thesunboy.com/
 # GitSource: https://github.com/hx940929/linuxShell
-# Created Time: 2019-2-27-下午12:00
+# Created Time: 2019-2-28-下午12:50
 #---------------------说明--------------------------
-#
+# build docker
 #---------------------------------------------------
-echo "修复代码."
-whoami
-echo ${PUB_SHELLS}
+#先尝试使用low版的 sh调用docker打包镜像,
+#等等在试试gitlab提供的docker方式.
+docker build --file testContext/dockerfile.df --rm --tag pydevops:v1.0 .
