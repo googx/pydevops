f33d05e52e8e84ee51d1f77d28f352268e17b793
hx940929 <836845967@qq.com>
2019-02-28 16:03:52 +0800

    docker: build image successful. now run that image

--- build.sh
+++ build.sh
@@ -10,4 +10,33 @@
 #---------------------------------------------------
 #先尝试使用low版的 sh调用docker打包镜像,
 #等等在试试gitlab提供的docker方式.
-docker build --file testContext/dockerfile.df --rm --tag pydevops:v1.0 .
+
+function build() {
+    echo "build pydevops:v1.0 ing ...."
+    resuMsg=$(docker build --file testContext/dockerfile.df --tag pydevops:v1.0 .)
+    resuCode=$?
+    echo "build complete pydevops:v1.0....[$resuCode]:${resuMsg}"
+    return ${resuCode}
+}
+
+function run() {
+    echo "run pydevops:v1.0 ing ...."
+#    如果在docker run 后面追加-d=true或者-d，则containter将会运行在后台模式(Detached mode)。此时所有I/O数据只能通过网络资源或者共享卷组来进行交互。因为container不再监听你执行docker run的这个终端命令行窗口。但你可以通过执行docker attach 来重新挂载这个container里面。需要注意的时，如果你选择执行-d使container进入后台模式，那么将无法配合"--rm"参数。
+    resucode=$(docker stop pydevops_10)
+    echo $resucode
+    resuMsg=$(docker run -d --name pydevops_10 --publish 8888:8888 pydevops:v1.0 )
+    resurunCode=$?
+    echo "run complete pydevops:v1.0....[$resurunCode]: ${resuMsg}"
+    return ${resurunCode}
+}
+
+case $1 in
+    "build")
+        build
+        exit $?
+   ;;
+   "run")
+        run
+        exit $?
+   ;;
+esac
